d848dff4ec0d5116a9b84c6b72a392a44d2000ae
TheProgxy <theprogxy@gmail.com>
2025-11-08 15:16:40 +0100

    add mul basic

--- chonky_nums.h
+++ chonky_nums.h
@@ -28,21 +28,25 @@ typedef unsigned char      u8;
 typedef unsigned short     u16;
 typedef unsigned int       u32;
 typedef unsigned long long u64;
+typedef unsigned __int128  u128;
 
 typedef char      s8;
 typedef short     s16;
 typedef int       s32;
 typedef long long s64;
+typedef __int128  s128;
 
-STATIC_ASSERT(sizeof(u8)   == 1,  "u8 must be 1 byte");
-STATIC_ASSERT(sizeof(u16)  == 2,  "u16 must be 2 bytes");
-STATIC_ASSERT(sizeof(u32)  == 4,  "u32 must be 4 bytes");
-STATIC_ASSERT(sizeof(u64)  == 8,  "u64 must be 8 bytes");
+STATIC_ASSERT(sizeof(u8)   == 1,   "u8   must be 1  byte");
+STATIC_ASSERT(sizeof(u16)  == 2,   "u16  must be 2  bytes");
+STATIC_ASSERT(sizeof(u32)  == 4,   "u32  must be 4  bytes");
+STATIC_ASSERT(sizeof(u64)  == 8,   "u64  must be 8  bytes");
+STATIC_ASSERT(sizeof(u128) == 16,  "u128 must be 16 bytes");
 
-STATIC_ASSERT(sizeof(s8)   == 1,  "s8 must be 1 byte");
-STATIC_ASSERT(sizeof(s16)  == 2,  "s16 must be 2 bytes");
-STATIC_ASSERT(sizeof(s32)  == 4,  "s32 must be 4 bytes");
-STATIC_ASSERT(sizeof(s64)  == 8,  "s64 must be 8 bytes");
+STATIC_ASSERT(sizeof(s8)   == 1,  "s8   must be 1  byte");
+STATIC_ASSERT(sizeof(s16)  == 2,  "s16  must be 2  bytes");
+STATIC_ASSERT(sizeof(s32)  == 4,  "s32  must be 4  bytes");
+STATIC_ASSERT(sizeof(s64)  == 8,  "s64  must be 8  bytes");
+STATIC_ASSERT(sizeof(s128) == 16, "s128 must be 16 bytes");
 
 typedef u8 bool;
 
@@ -68,12 +72,12 @@ typedef u8 bool;
 
 	#define COLOR_STR(str, COLOR) COLOR str RESET_COLOR
 
-	#include "./str_error.h"
+	/* #include "./str_error.h" */
+	/* #define PERROR_LOG(format, ...) printf(COLOR_STR("WARNING:" __FILE__ ":%u: ", WARNING_COLOR) format ", because: " COLOR_STR("'%s'", BRIGHT_YELLOW) ".\n", __LINE__, ##__VA_ARGS__, str_error()) */
 	
 	#define ERROR_LOG(format, error_str, ...) printf(COLOR_STR("ERROR:%s:" __FILE__ ":%u: ", ERROR_COLOR) format "\n", error_str, __LINE__, ##__VA_ARGS__)
 	#define WARNING_LOG(format, ...)          printf(COLOR_STR("WARNING:" __FILE__ ":%u: ", WARNING_COLOR) format "\n", __LINE__, ##__VA_ARGS__)
 	#define INFO_LOG(format, ...)             printf(COLOR_STR("INFO:" __FILE__ ":%u: ", INFO_COLOR) format "\n", __LINE__, ##__VA_ARGS__)
-	#define PERROR_LOG(format, ...)           printf(COLOR_STR("WARNING:" __FILE__ ":%u: ", WARNING_COLOR) format ", because: " COLOR_STR("'%s'", BRIGHT_YELLOW) ".\n", __LINE__, ##__VA_ARGS__, str_error())
 	#ifdef _DEBUG
 		#define DEBUG_LOG(format, ...)            printf(COLOR_STR("DEBUG:" __FILE__ ":%u: ", DEBUG_COLOR) format "\n", __LINE__, ##__VA_ARGS__)
 	#else 
@@ -201,11 +205,10 @@ bool chonky_is_gt(BigNum a, BigNum b) {
 }
 
 BigNum __chonky_add(BigNum res, BigNum a, BigNum b) {
-	const u64 size = MAX(a.size, b.size);
-	const u64 size_64 = size / 8; 
+	const u64 size_64 = res.size / 8; 
 	
 	u64 carry = 0;
-	for (u64 i = 0; i <= size_64; ++i) {
+	for (u64 i = 0; i < size_64; ++i) {
 		u64* acc = (u64*) (res.data + i * 8);
 		const u64 a_val = (i < a.size) ? ((u64*) a.data)[i] : 0;
 		const u64 b_val = (i < b.size) ? ((u64*) b.data)[i] : 0;
@@ -216,11 +219,10 @@ BigNum __chonky_add(BigNum res, BigNum a, BigNum b) {
 }
 
 BigNum __chonky_sub(BigNum res, BigNum a, BigNum b) {
-	const u64 size = MAX(a.size, b.size);
-	const u64 size_64 = size / 8; 
+	const u64 size_64 = res.size / 8; 
 	
 	u64 carry = 0;
-	for (u64 i = 0; i <= size_64; ++i) {
+	for (u64 i = 0; i < size_64; ++i) {
 		u64* acc = (u64*) (res.data + i * 8);
 		const u64 a_val = (i < a.size) ? ((u64*) a.data)[i] : 0;
 		const u64 b_val = (i < b.size) ? ((u64*) b.data)[i] : 0;
@@ -228,12 +230,21 @@ BigNum __chonky_sub(BigNum res, BigNum a, BigNum b) {
 	}
 	
 	if (res.sign) {
-		for (u64 i = 0; i <= size_64; ++i) {
+		for (u64 i = 0; i < size_64; ++i) {
 			u64* acc = (u64*) (res.data + i * 8);
 			*acc = ~*acc + 1;	
 		}
 	}
 
+	return res;
+}
+
+BigNum __chonky_mul_s(BigNum res, BigNum a, BigNum b) {
+	for (u64 i = 0; i < a.size / 8; ++i) {
+		for (u64 j = 0; j < b.size / 8; ++j) {
+			*((u128*) res.data + (i + j) * 8) += (u128) ((u64*) a.data)[i] * ((u64*) b.data)[j];
+		}
+	}
 	return res;
 }
 
@@ -243,8 +254,7 @@ BigNum chonky_add(BigNum a, BigNum b) {
 		return (BigNum) {0};
 	}
 
-	u64 size = MAX(a.size, b.size);
-	BigNum res = { .size = align_64(size + 1) };
+	BigNum res = { .size = align_64(MAX(a.size, b.size) + 1) };
 
 	res.data = calloc(res.size, sizeof(u8));
 	if (res.data == NULL) {
@@ -270,6 +280,48 @@ BigNum chonky_sub(BigNum a, BigNum b) {
 	BigNum res = chonky_add(a, b);
 	return res;
 }
+
+BigNum chonky_mul(BigNum a, BigNum b) {
+	if (a.data == NULL || b.data == NULL) {
+		WARNING_LOG("Parameters must be not null.");
+		return (BigNum) {0};
+	}
+
+	BigNum res = { .size = align_64(a.size + b.size) };
+
+	res.data = calloc(res.size, sizeof(u8));
+	if (res.data == NULL) {
+		WARNING_LOG("Failed to allocate data buffer.");
+		return (BigNum) {0};
+	}
+
+	res.sign = a.sign | b.sign;
+	
+	res = __chonky_mul_s(res, a, b);
+
+	return res;
+}
+
+/* BigNum chonky_div(BigNum a, BigNum b) { */
+/* 	if (a.data == NULL || b.data == NULL) { */
+/* 		WARNING_LOG("Parameters must be not null."); */
+/* 		return (BigNum) {0}; */
+/* 	} */
+
+/* 	BigNum res = { .size = align_64(MAX(a.size, b.size)) }; */
+
+/* 	res.data = calloc(res.size, sizeof(u8)); */
+/* 	if (res.data == NULL) { */
+/* 		WARNING_LOG("Failed to allocate data buffer."); */
+/* 		return (BigNum) {0}; */
+/* 	} */
+
+/* 	res.sign = a.sign | b.sign; */
+	
+/* 	res = __chonky_div_s(res, a, b); */
+
+/* 	return res; */
+/* } */
 
 #endif //_CHONKY_NUMS_H_
 
